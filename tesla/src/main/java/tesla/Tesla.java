d41ebf943700fb7c54195b8bdc32cb290e5e3c36
raghu <raoraghavendra100@gmail.com>
2018-05-09 16:57:18 +0530

    Initial Commit

---
+++ tesla/src/main/java/tesla/Tesla.java
@@ -0,0 +1,8 @@
+package tesla;
+
+public class Tesla {
+	public String sample() {
+		return "This is a sample file" ;
+	}
+
+}
---
+++ tesla/src/test/java/tesla/Tesla1.java
@@ -0,0 +1,14 @@
+package tesla;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tesla1 {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
